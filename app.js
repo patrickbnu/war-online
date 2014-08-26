17f87bca44cc75323b120d3b63c1c07cf471fd25
patrickbnu <patrick.bnu@gmail.com>
2014-08-25 21:03:56 -0300

    -

---
+++ app.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var BinaryServer, express, http, path, app, audio, server, bs;
+
+BinaryServer = require('binaryjs').BinaryServer;
+express      = require('express');
+http         = require('http');
+path         = require('path');
+app          = express();
+
+
+// all environments
+//app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'client')));
+
+// development only
+if ('development' == app.get('env')) {
+    app.use(express.errorHandler());
+}
+
+server = http.createServer(app);
+
+server.listen(3000, function () {
+    console.log('Audio Server started on http://0.0.0.0:3000');
+});
+
+bs = new BinaryServer({ port: 9000 });
+
+bs.on('connection', function (client) {
+    client.on('stream', function (stream, meta) {
+        switch(meta.event) {
+            case 'list':
+               // audio.list(stream, meta);
+                break;
+
+            case 'request':
+               // audio.request(client, meta);
+                break;
+        }
+    });
+});
--- client/js/chat.js
+++ client/js/chat.js
@@ -1,3 +1,4 @@
+/** @jsx React.DOM */
 
 var Chat = React.createClass({
 	//http://danialk.github.io/blog/2013/06/16/reactjs-and-socket-dot-io-chat-application/
